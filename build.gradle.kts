a1dd495e3cbbf02599b7d07215e3c17b79f87ba3
Anisio Lemos <anisiolemos@protonmail.ch>
2025-10-25 13:24:17 -0300

    chore: removing authentication because it stoped working and i dont know why

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 	id("org.springframework.boot") version "3.4.8"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.23"
-	kotlin("plugin.allopen") version "1.9.23"
 	id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
@@ -25,21 +24,17 @@ dependencies {
 	implementation("com.aallam.ulid:ulid-kotlin:1.4.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.github.wimdeblauwe:htmx-spring-boot-thymeleaf:4.0.1")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("com.auth0:java-jwt:4.4.0")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("io.mockk:mockk:1.13.10")
-    testImplementation("com.ninja-squad:springmockk:4.0.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -63,7 +58,6 @@ detekt {
 	toolVersion = "1.23.6"
 	buildUponDefaultConfig = true
 	allRules = false
-	autoCorrect = true
 	config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
 	baseline = file("$rootDir/config/detekt/baseline.xml")
 }
